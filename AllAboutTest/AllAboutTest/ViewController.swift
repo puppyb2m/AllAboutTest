66a83a1b778177f4ce6186264244fa4379605ccd
Shun Zhang <shunzhang@Shuns-MacBook-Pro.local>
2018-11-05 10:22:58 +0800

    init project

---
+++ AllAboutTest/AllAboutTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllAboutTest
+//
+//  Created by Shun Zhang on 05/11/2018.
+//  Copyright © 2018 Shun Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
